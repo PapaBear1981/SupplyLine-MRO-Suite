a36c2bed77408007b6ba4e19a01fc13e2be8d797
Chris Washburn <MadScientistDad@hotmail.com>
2025-05-18 16:25:23 -0700

    Fix version mismatch issues (Issue #31)

---
+++ frontend/src/utils/version.js
@@ -0,0 +1,8 @@
+/**
+ * Centralized version information for the application
+ * This file should be the single source of truth for the application version
+ */
+
+export const APP_VERSION = '3.5.1';
+
+export default APP_VERSION;
