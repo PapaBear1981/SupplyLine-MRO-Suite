0cf24940d68b5c359c8fc51caf3470c7e2ebb78a
Chris <107969875+PapaBear1981@users.noreply.github.com>
2025-10-26 10:40:57 -0700

    Update application version to v5.0.0
    - Updated frontend/src/utils/version.js to display v5.0.0 to users
    - Updated frontend/package.json version to v5.0.0
    - Version is displayed in footer of MainLayout component

--- frontend/src/utils/version.js
+++ frontend/src/utils/version.js
@@ -3,6 +3,6 @@
  * This file should be the single source of truth for the application version
  */
 
-export const APP_VERSION = '3.5.3';
+export const APP_VERSION = '5.0.0';
 
 export default APP_VERSION;
